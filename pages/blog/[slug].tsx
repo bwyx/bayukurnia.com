e5048851384d4b37e538a4e3122eba5ef39fc11d
Bayu Kurnia <bwyx@users.noreply.github.com>
2021-12-30 10:23:35 +0700

    blog index

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,66 +1,60 @@
 import Block from '~/components/Block'
 import notion from '~/lib/notion'
+import config from '~/config'
 
-const Post = ({ postData, blocks }: any) => {
+const Post = ({ post, blocks }: any) => {
   return (
-    <div>
-      <h1>{postData?.properties?.title?.title[0]?.plain_text}</h1>
-      {/* <pre>{JSON.stringify(postData, null, 2)}</pre> */}
-      {blocks?.results?.length
-        ? blocks.results.map((block: any) => (
-            <Block key={block.id} block={block} />
-          ))
+    <article>
+      <h1 style={{ color: 'red', fontSize: '5rem' }}>
+        {post?.properties?.title?.title[0]?.plain_text}
+      </h1>
+      {blocks?.length
+        ? blocks.map((block: any) => <Block key={block.id} block={block} />)
         : null}
-    </div>
+    </article>
   )
 }
 
-export const getStaticProps = async ({ params }: any) => {
-  const { slug } = params!
-
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID as string,
+export const getStaticProps = async (req: any) => {
+  const { results: postResults } = await notion.databases.query({
+    database_id: config.NOTION_BLOG_DATABASE_ID,
     filter: {
-      or: [
+      and: [
         {
-          property: 'slug',
-          text: {
-            equals: slug
+          property: 'published',
+          checkbox: {
+            equals: true
           }
         },
         {
-          property: 'slugged',
+          property: 'slug',
           text: {
-            equals: slug
+            equals: req.params.slug
           }
         }
       ]
     }
   })
 
-  const postData = response.results[0]
-
-  const blocks = await notion.blocks.children.list({
-    block_id: postData.id
+  const { results: blockResults } = await notion.blocks.children.list({
+    block_id: postResults[0].id
   })
 
   return {
     props: {
-      postData,
-      blocks
-    }
+      post: postResults[0],
+      blocks: blockResults
+    },
+    revalidate: 10
   }
 }
 
 export const getStaticPaths = async () => {
   const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID as string
+    database_id: config.NOTION_BLOG_DATABASE_ID
   })
 
-  const slug = response.results.map((post: any) => {
-    const { slug, slugged } = post.properties
-    return slug.url || slugged.formula.string
-  })
+  const slug = response.results.map((post: any) => post.properties.slug.url)
 
   return {
     paths: slug.map((s: string) => '/blog/' + s),
