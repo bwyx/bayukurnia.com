0d0676d4fc30889750ac54306e6ab0c3e8de20b5
Bayu Kurnia <bwyx@users.noreply.github.com>
2021-12-30 11:25:51 +0700

    added inter font

--- src/styles/global.style.ts
+++ src/styles/global.style.ts
@@ -1,5 +1,13 @@
 import { globalCss } from '@stitches/react'
 
-const globalStyles = globalCss({})
+const globalStyles = globalCss({
+  html: {
+    fontFeatureSettings: "'cv02','cv03','cv04','cv11'"
+  },
+  body: {
+    fontFamily: '$sans',
+    color: '$text'
+  }
+})
 
 export default globalStyles
