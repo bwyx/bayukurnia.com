21e6e3c74ec19fb4c591255d808fa4a1161426da
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-01-13 01:20:48 +0700

    drop `@stitches/react`, use `@stitches/core`

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,47 +1,46 @@
-import { createStitches } from '@stitches/react'
+import { createStitches } from '@stitches/core'
 
-export const { styled, css, getCssText, globalCss, createTheme } =
-  createStitches({
-    theme: {
-      space: {},
-      fontSizes: {
-        xs: '0.75rem',
-        sm: '0.875rem',
-        base: '1rem',
-        lg: '1.125rem',
-        xl: '1.25rem',
-        '2xl': '1.5rem',
-        '3xl': '1.875rem',
-        '4xl': '2.25rem',
-        '5xl': '3rem',
-        '6xl': '3.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6rem',
-        '9xl': '8rem'
-      },
-      fonts: {
-        sans: "Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Noto Color Emoji', 'Segoe UI Symbol'",
-        serif: "ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
-        mono: "Iosevka, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-        untitled: 'Untitled Sans, apple-system, sans-serif'
-      },
-      radii: {
-        none: '0px',
-        sm: '0.125rem',
-        DEFAULT: '0.25rem',
-        md: '0.375rem',
-        lg: '0.5rem',
-        xl: '0.75rem',
-        '2xl': '1rem',
-        '3xl': '1.5rem',
-        full: '9999px'
-      }
+export const { css, getCssText, globalCss, createTheme } = createStitches({
+  theme: {
+    space: {},
+    fontSizes: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem'
     },
-    media: {
-      sm: '(min-width: 640px)',
-      md: '(min-width: 768px)',
-      lg: '(min-width: 1024px)',
-      xl: '(min-width: 1280px)',
-      '2xl': '(min-width: 1536px)'
+    fonts: {
+      sans: "Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Noto Color Emoji', 'Segoe UI Symbol'",
+      serif: "ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
+      mono: "Iosevka, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+      untitled: 'Untitled Sans, apple-system, sans-serif'
+    },
+    radii: {
+      none: '0px',
+      sm: '0.125rem',
+      DEFAULT: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
+      full: '9999px'
     }
-  })
+  },
+  media: {
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 1024px)',
+    xl: '(min-width: 1280px)',
+    '2xl': '(min-width: 1536px)'
+  }
+})
