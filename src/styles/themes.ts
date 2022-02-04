00d28bb2c31345b95caf9cc1da701c804a0c28ee
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-04 19:05:27 +0700

    fix rgb token

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -4,9 +4,9 @@ export const lightTheme = {
   rgb: {
     brand: '121 116 14',
     accent: '181 118 20',
-    fg1: '56 58 54',
-    fg2: '84 87 82',
-    fg3: '140 144 137',
+    fg1: '$rgb$text-800',
+    fg2: '$rgb$text-700',
+    fg3: '$rgb$text-600',
     bg: '250 250 250',
     glass: '$brand'
   }
@@ -16,9 +16,9 @@ export const darkTheme = {
   rgb: {
     brand: '184 187 38',
     accent: '250 189 47',
-    fg1: '242 243 242',
-    fg2: '191 193 189',
-    fg3: '140 144 137',
+    fg1: '$rgb$text-100',
+    fg2: '$rgb$text-300',
+    fg3: '$rgb$text-500',
     bg: '16 17 14',
     glass: '255 255 255'
   }
