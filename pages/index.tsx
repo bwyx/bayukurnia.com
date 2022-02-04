faa8a409763591b30337f2fb448df537e3764450
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-04 21:49:06 +0700

    move author to footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
-import { getPlaiceholder } from 'plaiceholder'
-
 import { attachMainLayout } from '~/layouts/Main.layout'
-import { BlurImage, VisuallyHidden } from '~/components'
 import { PostCard } from '~/components/blog'
-import GitHubLogo from '~/components/vectors/GitHubLogo'
-import TwitterLogo from '~/components/vectors/TwitterLogo'
 
-import { css } from '~/styles'
 import container from '~/styles/container.style'
 import stack from '~/styles/stack.style'
 
@@ -19,131 +13,28 @@ import type { PostResult } from '~/types/notion.type'
 
 interface PageProps {
   posts: PostProperties[]
-  authorImage: {
-    blurDataURL: string
-    src: string
-  }
 }
 
 const styles = {
-  about: stack({
-    dir: {
-      '@initial': 'col',
-      '@sm': 'row'
-    },
-    y: 'center',
-    css: {
-      xBackground: '$brand',
-      xBackgroundOpacity: 0.1,
-      p: '$8',
-      margin: '$8 auto',
-      borderRadius: '$3xl',
-      gap: '$6'
-    }
-  }),
-  aboutImage: css({
-    boxShadow: '0 0px 0px 2px rgb($rgb$bg), 0 0 0px 4px rgb($rgb$brand)',
-    borderRadius: '$full',
-    display: 'inherit',
-    overflow: 'hidden',
-    margin: '0 auto'
-  })(),
-  aboutText: stack({
-    x: 'center',
-    y: 'center',
-    density: 'spaceBetween',
-    grow: true,
-    css: {
-      lineHeight: '$tight'
-    }
-  }),
-  greet: stack({
-    dir: 'col',
-    css: {
-      xColor: '$fg2',
-      fontSize: '$sm',
-      '@sm': {
-        fontSize: '$base'
-      },
-      '& strong': {
-        xColor: '$fg1',
-        fontSize: '$xl',
-        '@sm': {
-          fontSize: '$2xl'
-        },
-        '@md': {
-          fontSize: '$3xl'
-        }
-      }
-    }
-  }),
-  socials: stack({
-    css: {
-      gap: '$4',
-      mt: '$1',
-      '& a:hover': {
-        xColor: '$brand'
-      },
-      '& svg': {
-        width: 18,
-        height: 18,
-        '@md': {
-          width: 24,
-          height: 24
-        }
-      }
-    }
-  }),
   posts: stack({
     dir: 'col',
     css: {
-      my: '$16'
+      my: '$8'
     }
   })
 }
 
-const Home = ({ posts, authorImage }: PageProps) => {
+const Home = ({ posts }: PageProps) => {
   return (
-    <div className={container({ size: 'small' })}>
-      <div className={styles.about}>
-        <div className={styles.aboutImage}>
-          <BlurImage
-            {...authorImage}
-            width={88}
-            height={88}
-            alt="Bayu Kurnia's Picture"
-          />
-        </div>
-        <div className={styles.aboutText}>
-          <p className={styles.greet}>
-            <span>Halo,</span>
-            <strong>Bayu Kurnia</strong>
-            <span>disini.</span>
-          </p>
-          <ul className={styles.socials}>
-            <li>
-              <a
-                href="https://twitter.com/0x4b70"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <VisuallyHidden>0x4b70&apos;s Twitter</VisuallyHidden>
-                <TwitterLogo />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/bwyx"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <VisuallyHidden>bwyx&apos;s GitHub</VisuallyHidden>
-                <GitHubLogo />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div
+      className={`${container({ size: 'small' })} ${stack({
+        dir: 'col',
+        density: 'spaceBetween',
+        css: {
+          height: '100%'
+        }
+      })}`}
+    >
       <div className={styles.posts}>
         {posts.map((props) => (
           <PostCard key={props.id} {...props} />
@@ -162,14 +53,8 @@ export const getStaticProps: GetStaticProps<PageProps> = async () => {
     results.map((post) => getPostData(post as PostResult))
   )
 
-  const { base64, img } = await getPlaiceholder('/bayukurnia.png')
-  const authorImage = {
-    blurDataURL: base64,
-    ...img
-  }
-
   return {
-    props: { posts, authorImage },
+    props: { posts },
     revalidate: process.env.ENV === 'local' ? 1 : 300
   }
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,133 @@
-import { css } from '~/styles'
+import { BlurImage, VisuallyHidden } from '~/components'
+import GitHubLogo from '~/components/vectors/GitHubLogo'
+import TwitterLogo from '~/components/vectors/TwitterLogo'
+import SquareGlass from '~/components/vectors/SquareGlass'
 
-const footer = css({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '1rem 0',
-  '& p': {
+import { css } from '~/styles'
+import stack from '~/styles/stack.style'
+
+const styles = {
+  footer: stack({
+    x: 'center',
+    dir: 'col',
+    css: {
+      overflow: 'hidden'
+    }
+  }),
+  about: stack({
+    y: 'bottom',
+    x: 'center',
+    dir: 'col',
+    css: {
+      p: '$4',
+      margin: '$8 auto',
+      borderRadius: '$3xl',
+      gap: '$6',
+      position: 'relative',
+      pointerEvents: 'none',
+      width: 240,
+      height: 240,
+      zIndex: 1
+    }
+  }),
+  aboutImage: css({
+    outline: '2px solid rgb($rgb$brand)',
+    outlineOffset: '2px',
+    borderRadius: '$full',
+    display: 'inherit',
+    overflow: 'hidden',
+    margin: '0 auto'
+  })(),
+  aboutText: stack({
+    x: 'center',
+    y: 'center',
+    density: 'spaceBetween',
+    css: {
+      position: 'relative',
+      pointerEvents: 'auto',
+      py: '$2',
+      width: '100%',
+      lineHeight: '$tight',
+      zIndex: 1
+    }
+  }),
+  greet: stack({
+    dir: 'col',
+    css: {
+      xColor: '$fg2',
+      fontSize: '$xs',
+      '& strong': {
+        xColor: '$fg1',
+        fontSize: '$xl'
+      }
+    }
+  }),
+  socials: stack({
+    css: {
+      xColor: '$fg2',
+      gap: '$4',
+      mt: '$1',
+      '& a:hover': {
+        xColor: '$brand'
+      },
+      '& svg': {
+        width: 18,
+        height: 18
+      }
+    }
+  }),
+  credit: css({
+    p: '$4',
     fontSize: '0.8rem',
     xColor: '$fg3'
-  }
-})()
+  })()
+}
 
 const Footer = () => {
   return (
-    <footer className={footer}>
-      <p>© Bayu Kurnia 2021</p>
+    <footer className={styles.footer}>
+      <div className={styles.about}>
+        <SquareGlass />
+        <div className={styles.aboutImage}>
+          <BlurImage
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAP0lEQVQImQE0AMv/AAUEDwEACSsrM7W1xACSk6L/+/neyshtbnwASUpW9uXmrpuYFxgfAEE/QjAqMQEAByMjKRJ7Evg6EQWIAAAAAElFTkSuQmCC"
+            src="/bayukurnia.png"
+            width={88}
+            height={88}
+            alt="Bayu Kurnia's Picture"
+          />
+        </div>
+        <div className={styles.aboutText}>
+          <p className={styles.greet}>
+            <span>Halo,</span>
+            <strong>Bayu Kurnia</strong>
+            <span>disini.</span>
+          </p>
+          <ul className={styles.socials}>
+            <li>
+              <a
+                href="https://twitter.com/0x4b70"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <VisuallyHidden>0x4b70&apos;s Twitter</VisuallyHidden>
+                <TwitterLogo />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/bwyx"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <VisuallyHidden>bwyx&apos;s GitHub</VisuallyHidden>
+                <GitHubLogo />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <p className={styles.credit}>© Bayu Kurnia 2021</p>
     </footer>
   )
 }
