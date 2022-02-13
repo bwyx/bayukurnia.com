0c60c613b19be8f94815c085c55913fee074f820
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-13 18:48:14 +0700

    respect `prose` style

--- src/components/mdx/Pre.tsx
+++ src/components/mdx/Pre.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import prism from '~/styles/prism.style'
 
-const styles = prism({
-  theme: 'gruvbox',
-  size: {
-    '@initial': 'small',
-    '@md': 'base'
-  }
-})
+const styles = prism({ theme: 'gruvbox' })
 
 const Pre = ({ children }: { children: React.ReactNode }) => {
   return <pre className={styles}>{children}</pre>
