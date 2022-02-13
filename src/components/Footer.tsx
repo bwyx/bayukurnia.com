16889fbfddb1cafa480448418e1afeff7230954d
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-13 19:37:12 +0700

    remove footer profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,3 @@
-import { BlurImage, VisuallyHidden } from '~/components'
-import GitHubLogo from '~/components/vectors/GitHubLogo'
-import TwitterLogo from '~/components/vectors/TwitterLogo'
-import SquareGlass from '~/components/vectors/SquareGlass'
-import config from '~/config'
-
 import { css } from '~/styles'
 import stack from '~/styles/stack.style'
 
@@ -13,68 +7,6 @@ const styles = {
     dir: 'col',
     css: {
       overflow: 'hidden'
-    }
-  }),
-  about: stack({
-    y: 'bottom',
-    x: 'center',
-    dir: 'col',
-    css: {
-      padding: '$4',
-      margin: '$8 auto',
-      borderRadius: '$3xl',
-      gap: '$6',
-      position: 'relative',
-      pointerEvents: 'none',
-      width: 240,
-      height: 240,
-      zIndex: 1
-    }
-  }),
-  aboutImage: css({
-    outline: '2px solid rgb($rgb$brand)',
-    outlineOffset: '2px',
-    borderRadius: '$full',
-    display: 'inherit',
-    overflow: 'hidden',
-    margin: '0 auto'
-  })(),
-  aboutText: stack({
-    x: 'center',
-    y: 'center',
-    density: 'spaceBetween',
-    css: {
-      position: 'relative',
-      pointerEvents: 'auto',
-      py: '$2',
-      width: '100%',
-      lineHeight: '$tight',
-      zIndex: 1
-    }
-  }),
-  greet: stack({
-    dir: 'col',
-    css: {
-      xColor: '$fg2',
-      fontSize: '$xs',
-      '& strong': {
-        xColor: '$fg1',
-        fontSize: '$xl'
-      }
-    }
-  }),
-  socials: stack({
-    css: {
-      xColor: '$fg2',
-      gap: '$4',
-      mt: '$1',
-      '& a:hover': {
-        xColor: '$brand'
-      },
-      '& svg': {
-        width: 18,
-        height: 18
-      }
     }
   }),
   credit: css({
@@ -87,41 +19,6 @@ const styles = {
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.about}>
-        <SquareGlass />
-        <div className={styles.aboutImage}>
-          <BlurImage {...config.avatar} width={88} height={88} />
-        </div>
-        <div className={styles.aboutText}>
-          <p className={styles.greet}>
-            <span>Halo,</span>
-            <strong>Bayu Kurnia</strong>
-            <span>disini.</span>
-          </p>
-          <ul className={styles.socials}>
-            <li>
-              <a
-                href="https://twitter.com/0x4b70"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <VisuallyHidden>0x4b70&apos;s Twitter</VisuallyHidden>
-                <TwitterLogo />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/bwyx"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <VisuallyHidden>bwyx&apos;s GitHub</VisuallyHidden>
-                <GitHubLogo />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
       <p className={styles.credit}>© Bayu Kurnia 2021</p>
     </footer>
   )
