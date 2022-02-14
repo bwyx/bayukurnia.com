3dc7a8394f3828221b64f68997fd46ef2350c26e
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-14 07:55:51 +0700

    provide negative `space` typings

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,12 +3,34 @@ import { darkTheme, lightTheme } from './themes'
 import type * as Stitches from '@stitches/core'
 
 export type CSS = Stitches.CSS<typeof config>
-type Space = Stitches.ScaleValue<'space'>
+type Space = Stitches.ScaleValue<'space'> | `-$${keyof typeof space}`
 type RGBS = keyof typeof darkTheme.rgb & keyof typeof lightTheme.rgb
 type RGB = `$${RGBS}` | Stitches.ScaleValue<'rgb'>
 type Opacity = Stitches.PropertyValue<'opacity'>
 
 export type RGBColor = keyof typeof config.theme.rgb
+
+const space = {
+  px: '1px',
+  0: '0',
+  1: '0.25rem',
+  2: '0.5rem',
+  3: '0.75rem',
+  4: '1rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  8: '2rem',
+  10: '2.5rem',
+  12: '3rem',
+  16: '4rem',
+  20: '5rem',
+  24: '6rem',
+  32: '8rem',
+  40: '10rem',
+  48: '12rem',
+  56: '14rem',
+  64: '16rem'
+}
 
 export const { css, keyframes, getCssText, globalCss, createTheme, config } =
   createStitches({
@@ -95,27 +117,7 @@ export const { css, keyframes, getCssText, globalCss, createTheme, config } =
         full: '100%',
         screenHeight: '100vh'
       },
-      space: {
-        px: '1px',
-        0: '0',
-        1: '0.25rem',
-        2: '0.5rem',
-        3: '0.75rem',
-        4: '1rem',
-        5: '1.25rem',
-        6: '1.5rem',
-        8: '2rem',
-        10: '2.5rem',
-        12: '3rem',
-        16: '4rem',
-        20: '5rem',
-        24: '6rem',
-        32: '8rem',
-        40: '10rem',
-        48: '12rem',
-        56: '14rem',
-        64: '16rem'
-      },
+      space,
       zIndices: {
         auto: 'auto',
         0: '0',
