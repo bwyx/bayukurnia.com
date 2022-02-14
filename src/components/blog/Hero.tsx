41e1068e97138d59c99c313896fc214e96015c4e
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-14 07:28:56 +0700

    added socials to hero

--- src/components/blog/Hero.tsx
+++ src/components/blog/Hero.tsx
@@ -1,11 +1,13 @@
 import { format, parseISO } from 'date-fns'
-import { BlurImage } from '~/components'
+import { BlurImage, VisuallyHidden } from '~/components'
 import SquareGlass from '~/components/vectors/SquareGlass'
 import config from '~/config'
 
 import styles from '~/styles/components/Hero.style'
 
 import type { PostWithCover } from '~/types/blog.type'
+import GitHubLogo from '../vectors/GitHubLogo'
+import TwitterLogo from '../vectors/TwitterLogo'
 
 const Hero = ({ title, summary, publishedAt, cover }: PostWithCover) => {
   const { width, height, ...img } = cover
@@ -13,6 +15,9 @@ const Hero = ({ title, summary, publishedAt, cover }: PostWithCover) => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
+        <div className={styles.publishedDate}>
+          {format(parseISO(publishedAt), 'LLLL d, yyyy')}
+        </div>
         <div className={styles.postDetails}>
           <h1 className={styles.postTitle}>{title}</h1>
           <p className={styles.postSnippet}>{summary}</p>
@@ -23,9 +28,26 @@ const Hero = ({ title, summary, publishedAt, cover }: PostWithCover) => {
           </div>
           <div className={styles.authorDetails}>
             <strong className={styles.authorName}>Bayu Kurnia</strong>
-            <time className={styles.publishedDate} dateTime={publishedAt}>
-              {format(parseISO(publishedAt), 'LLLL d, yyyy')}
-            </time>
+            <div className={styles.socials}>
+              <a
+                className="twitter"
+                href="https://twitter.com/0x4b70"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <VisuallyHidden>0x4b70&apos;s Twitter</VisuallyHidden>
+                <TwitterLogo />
+              </a>
+              <a
+                className="github"
+                href="https://github.com/bwyx"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <VisuallyHidden>bwyx&apos;s GitHub</VisuallyHidden>
+                <GitHubLogo />
+              </a>
+            </div>
           </div>
         </div>
         <figure className={styles.cover}>
