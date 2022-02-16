4c7fcebc2763bac2956b1e1c9b5cb0f403d876ed
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-16 12:35:40 +0700

    refactor, use css local variables

--- src/styles/prism.style.ts
+++ src/styles/prism.style.ts
@@ -26,86 +26,75 @@ const prism = css({
     theme: {
       gruvbox: {
         [`.${themes.dark} &`]: {
-          // Default to Gruvbox Dark
-          color: '#ebdbb2',
-          background: '#1d2021',
+          '$$prism-fg': '#ebdbb2',
+          '$$prism-bg': '#1d2021',
 
-          // Selection
-          '::selection, & ::selection, ::-moz-selection, & ::-moz-selection': {
-            color: '#fbf1c7',
-            background: '#7c6f64'
-          },
+          '$$prism-fg-selection': '#fbf1c7',
+          '$$prism-bg-selection': '#7c6f64',
 
-          // Tokens
-          '.token.comment, .token.prolog, .token.cdata': { color: '#a89984' },
-          '.token.delimiter, .token.boolean, .token.keyword, .token.selector, .token.important, .token.atrule':
-            {
-              color: '#fb4934'
-            },
-          '.token.operator, .token.punctuation, .token.attr-name': {
-            color: '#a89984'
-          },
-          '.token.tag, .token.tag .punctuation, .token.doctype, .token.builtin':
-            {
-              color: '#fabd2f'
-            },
-          '.token.entity, .token.number, .token.symbol': { color: '#d3869b' },
-          '.token.property, .token.constant, .token.variable': {
-            color: '#fb4934'
-          },
-          '.token.string, .token.char': { color: '#b8bb26' },
-          '.token.attr-value, .token.attr-value .punctuation': {
-            color: '#a89984'
-          },
-          '.token.url': { color: '#b8bb26', textDecoration: 'underline' },
-          '.token.function': { color: '#fabd2f' },
-          '.token.regex': { background: '#b8bb26' },
-          '.token.bold': { fontWeight: 'bold' },
-          '.token.italic': { fontStyle: 'italic' },
-          '.token.inserted': { background: '#a89984' },
-          '.token.deleted': { background: '#fb4934' }
+          $$syntax1: '#a89984',
+          $$syntax2: '#fb4934',
+          $$syntax3: '#fabd2f',
+          $$syntax4: '#d3869b',
+          $$syntax5: '#b8bb26'
         },
-        // Gruvox Light
+
         [`.${themes.light} &`]: {
-          color: '#3c3836',
-          xBackground: '$brand',
-          xBackgroundOpacity: 0.05,
+          '$$prism-fg': '#3c3836',
+          '$$prism-bg': 'rgb($rgb$brand / 0.05)',
 
-          // Selection
-          '::selection, & ::selection, ::-moz-selection, & ::-moz-selection': {
-            color: '#282828',
-            background: '#a89984'
+          '$$prism-fg-selection': '#282828',
+          '$$prism-bg-selection': '#a89984',
+
+          $$syntax1: '#7c6f64',
+          $$syntax2: '#9d0006',
+          $$syntax3: '#b57614',
+          $$syntax4: '#8f3f71',
+          $$syntax5: '#797403'
+        },
+
+        color: '$$prism-fg',
+        background: '$$prism-bg',
+
+        // Selection; note: don't combine -moz prefix, it'll break the on non-Firefox browsers
+        'code[class*="language-"]::-moz-selection, code[class*="language-"] ::-moz-selection':
+          {
+            color: '$$prism-fg-selection',
+            background: '$$prism-bg-selection'
+          },
+        'code[class*="language-"]::selection, code[class*="language-"] ::selection':
+          {
+            color: '$$prism-fg-selection',
+            background: '$$prism-bg-selection'
           },
 
-          // Tokens
-          '.token.comment, .token.prolog, .token.cdata': { color: '#7c6f64' },
-          '.token.delimiter, .token.boolean, .token.keyword, .token.selector, .token.important, .token.atrule':
-            {
-              color: '#9d0006'
-            },
-          '.token.operator, .token.punctuation, .token.attr-name': {
-            color: '#7c6f64'
+        // Tokens
+        '.token.comment, .token.prolog, .token.cdata': { color: '$$syntax1' },
+        '.token.delimiter, .token.boolean, .token.keyword, .token.selector, .token.important, .token.atrule':
+          {
+            color: '$$syntax2'
           },
-          '.token.tag, .token.tag .punctuation, .token.doctype, .token.builtin':
-            {
-              color: '#b57614'
-            },
-          '.token.entity, .token.number, .token.symbol': { color: '#8f3f71' },
-          '.token.property, .token.constant, .token.variable': {
-            color: '#9d0006'
-          },
-          '.token.string, .token.char': { color: '#797403' },
-          '.token.attr-value, .token.attr-value .punctuation': {
-            color: '#7c6f64'
-          },
-          '.token.url': { color: '#797403', textDecoration: 'underline' },
-          '.token.function': { color: '#b57614' },
-          '.token.regex': { background: '#797403' },
-          '.token.bold': { fontWeight: 'bold' },
-          '.token.italic': { fontStyle: 'italic' },
-          '.token.inserted': { background: '#7c6f64' },
-          '.token.deleted': { background: '#9d0006' }
-        }
+        '.token.operator, .token.punctuation, .token.attr-name': {
+          color: '$$syntax1'
+        },
+        '.token.tag, .token.tag .punctuation, .token.doctype, .token.builtin': {
+          color: '$$syntax3'
+        },
+        '.token.entity, .token.number, .token.symbol': { color: '$$syntax4' },
+        '.token.property, .token.constant, .token.variable': {
+          color: '$$syntax2'
+        },
+        '.token.string, .token.char': { color: '$$syntax5' },
+        '.token.attr-value, .token.attr-value .punctuation': {
+          color: '$$syntax1'
+        },
+        '.token.url': { color: '$$syntax5', textDecoration: 'underline' },
+        '.token.function': { color: '$$syntax3' },
+        '.token.regex': { background: '$$syntax5' },
+        '.token.bold': { fontWeight: 'bold' },
+        '.token.italic': { fontStyle: 'italic' },
+        '.token.inserted': { background: '$$syntax1' },
+        '.token.deleted': { background: '$$syntax2' }
       }
     }
   }
