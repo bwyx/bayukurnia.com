2e36c23578b9d5a5b4188f2ceeb0926ab8a1994d
Bayu Kurnia <bwyx@users.noreply.github.com>
2022-02-28 17:24:02 +0700

    wip Tab component

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,10 +1,12 @@
 import Pre from './Pre'
 import Callout from './Callout'
 import Text from './Text'
+import Tab from './Tab'
 
 const customComponents = {
   Text,
-  Callout
+  Callout,
+  Tab
 }
 
 const MDXComponents = {
