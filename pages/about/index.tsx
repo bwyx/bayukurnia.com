57c5057e8de073d0e8d9364901832405ae8b4064
Bayu Kurnia <hi@bayukurnia.com>
2022-03-04 06:43:45 +0700

    added seo

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import { attachMainLayout } from '~/layouts/Main.layout'
 
 import { container } from '~/styles/primitives'
@@ -7,6 +8,7 @@ import { Page } from '~/types/page.type'
 const About: Page = () => {
   return (
     <div className={container({ size: 'small' })}>
+      <NextSeo title="About" />
       Di sini juga ngga ada apa-apa.
     </div>
   )
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Header } from './Header'
 export { default as Footer } from './Footer'
-export { default as SEO } from './SEO'
 export { default as Article } from './Article'
 export { default as BlurImage } from './BlurImage'
 export { default as VisuallyHidden } from './VisuallyHidden'
