39810829f36bfcfeaca14b119da0f24ceecc0fce
Bayu Kurnia <hi@bayukurnia.com>
2022-03-15 06:06:46 +0700

    chat placeholder

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,3 +1,4 @@
 export { default as ChatBubble } from './ChatBubble'
+export { ChatBubblePlaceholder } from './ChatBubble'
 export { default as ChatInputForm } from './ChatInputForm'
 export { default as RecentlyMessageCounter } from './RecentlyMessageCounter'
