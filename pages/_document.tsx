ed6533aacdaf327236ee294e475cedc52e4849ed
Bayu Kurnia <hi@bayukurnia.com>
2022-07-17 22:17:37 +0700

    vanilla toggle theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { getCssText } from '~/styles'
+import { light, dark } from '~/styles/themes.css'
 
 export default class Document extends NextDocument {
   render() {
@@ -17,6 +18,11 @@ export default class Document extends NextDocument {
           <style dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
         <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(){function setTheme(newTheme){document.body.className=newTheme;window.__theme=newTheme;window.__onThemeChange(newTheme)}window.__onThemeChange=function(){};window.__setPreferredTheme=function(newTheme){setTheme(newTheme);try{localStorage.setItem("theme",JSON.stringify(window.__theme))}catch(err){}};const darkQuery=window.matchMedia("(prefers-color-scheme: dark)");darkQuery.addListener(function(event){window.__setPreferredTheme(event.matches?"${dark}":"${light}")});let preferredTheme;try{preferredTheme=JSON.parse(localStorage.getItem("theme"))}catch(err){}setTheme(preferredTheme||(darkQuery.matches?"${dark}":"${light}"))})();`
+            }}
+          />
           <Main />
           <NextScript />
         </body>
--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,59 +1,18 @@
-import { useState } from 'react'
-import config from '~/config'
-import { availableTheme, ThemeName } from '~/styles/themes'
+import { useEffect, useState } from 'react'
 
-const setLocalStorage = (key: string, value: string) => {
-  if (typeof window !== 'undefined') {
-    window.localStorage.setItem(key, value)
-  }
-}
-
-const getLocalStorage = <T extends string | null>(key: string) => {
-  if (typeof window !== 'undefined') {
-    return <T>window.localStorage.getItem(key)
-  }
-
-  return null
-}
-
-const getSavedTheme = (): ThemeName | null => {
-  try {
-    const savedTheme = getLocalStorage<ThemeName>('theme')
-    const isValid = savedTheme && availableTheme.includes(savedTheme)
-
-    return isValid ? savedTheme : null
-  } catch (e) {
-    // When Chrome in incognito, localStorage cannot be accessed
-    console.warn(e)
-    return null
-  }
-}
-
-const saveTheme = (theme: ThemeName) => {
-  try {
-    setLocalStorage('theme', theme)
-  } catch (e) {
-    console.log(e)
-  }
-}
+import { light, dark } from '~/styles/themes.css'
 
 const useTheme = () => {
-  const [themeIndex, setThemeIndex] = useState(
-    availableTheme.indexOf(getSavedTheme() || config.defaultTheme)
-  )
-  const theme = availableTheme[themeIndex]
-
-  const cycleTheme = () => {
-    const overIndex = themeIndex + 1 >= availableTheme.length
-
-    const nextIndex = overIndex ? 0 : themeIndex + 1
-    const nextTheme = availableTheme[nextIndex]
-
-    setThemeIndex(nextIndex)
-    saveTheme(nextTheme)
+  const [theme, setTheme] = useState(global.window?.__theme || light)
+  const toggleTheme = () => {
+    global.window.__setPreferredTheme(theme === light ? dark : light)
   }
 
-  return { theme, cycleTheme }
+  useEffect(() => {
+    global.window.__onThemeChange = setTheme
+  }, [])
+
+  return { theme, toggleTheme }
 }
 
 export default useTheme
