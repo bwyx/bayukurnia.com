6d3812a79f7c86b61b72cd46bbcca8cbc5290895
Bayu Kurnia <hi@bayukurnia.com>
2022-07-17 22:17:37 +0700

    vanilla toggle theme

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,33 +1,8 @@
-import { useEffect } from 'react'
 import { useTheme } from '~/hooks'
 import SunMoon from '~/components/vectors/SunMoon'
 
-import themes, { allThemeClass, ThemeName } from '~/styles/themes'
-
 const ThemeToggle = () => {
-  const { theme, cycleTheme } = useTheme()
-
-  const applyTheme = (theme: ThemeName) => {
-    const htmlClass = document.documentElement.classList
-
-    htmlClass.remove(...allThemeClass)
-    htmlClass.add(themes[theme].className)
-  }
-
-  useEffect(() => applyTheme(theme), [theme])
-
-  useEffect(() => {
-    // listen to all available scheme changes
-    for (const theme in themes) {
-      if (Object.prototype.hasOwnProperty.call(themes, theme)) {
-        const scheme = window.matchMedia(`(prefers-color-scheme: ${theme})`)
-
-        scheme.addEventListener('change', () => {
-          if (scheme.matches) applyTheme(theme as ThemeName)
-        })
-      }
-    }
-  }, [])
+  const { toggleTheme } = useTheme()
 
   return (
     <button
@@ -36,7 +11,7 @@ const ThemeToggle = () => {
       title="Toggles light & dark"
       aria-label="auto"
       aria-live="polite"
-      onClick={cycleTheme}
+      onClick={toggleTheme}
     >
       <SunMoon />
     </button>
---
+++ src/styles/themes.css.ts
@@ -0,0 +1,50 @@
+import { createTheme } from '@vanilla-extract/css'
+import { themeVars, rgb } from '~/styles/variables.css'
+
+export const light = createTheme(themeVars, {
+  rgb: {
+    brand: '121 116 14', // #79740e
+    accent: '181 118 20', // #b57614
+    fg1: rgb['text-800'],
+    fg2: rgb['text-700'],
+    fg3: rgb['text-600'],
+    bg: '250 250 250', // #fafafa
+    glass: rgb.brand,
+
+    // Colors
+    black: '40 40 40', // #282828
+    gray: '107 114 128', // #6b7280
+    brown: '214 93 14', // #d65d0e
+    orange: '254 128 25', // #fe8019
+    yellow: '181 118 20', // #b57614
+    green: '121 116 14', // #79740e
+    blue: '7 102 120', // #076678
+    purple: '177 98 134', // #b16286
+    pink: '211 134 155', // #d3869b
+    red: '157 0 6' // #9d0006
+  }
+})
+
+export const dark = createTheme(themeVars, {
+  rgb: {
+    brand: '184 187 38', // #b8bb26
+    accent: '250 189 47', // #fabd2f
+    fg1: rgb['text-100'],
+    fg2: rgb['text-300'],
+    fg3: rgb['text-500'],
+    bg: '16 17 14', // #10110e
+    glass: '255 255 255', // #ffffff
+
+    // Colors
+    black: '40 40 40', // #282828
+    gray: '107 114 128', // #6b7280
+    brown: '214 93 14', // #d65d0e
+    orange: '254 128 25', // #fe8019
+    yellow: '250 189 47', // #fabd2f
+    green: '184 187 38', // #b8bb26
+    blue: '131 165 152', // #83a598
+    purple: '177 98 134', // #b16286
+    pink: '211 134 155', // #d3869b
+    red: '251 73 52' // #fb4934
+  }
+})
