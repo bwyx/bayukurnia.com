b1fb01132ea17d1aa1e5a1289beec640b258f72f
Bayu Kurnia <hi@bayukurnia.com>
2022-07-18 22:53:03 +0700

    prose sizes & spacings

--- src/styles/primitives/prose.css.ts
+++ src/styles/primitives/prose.css.ts
@@ -1,9 +1,9 @@
 // styles from `@tailwindcss/typography`
 import { createVar, globalStyle, style } from '@vanilla-extract/css'
 
-import { em } from '~/styles/utils.css'
+import { round, rem, em } from '~/styles/utils.css'
 import { light, dark } from '~/styles/themes.css'
-import { rgb } from '~/styles/variables.css'
+import { media, rgb } from '~/styles/variables.css'
 
 const prose = style({})
 export default prose
@@ -334,4 +334,421 @@ globalStyle(`${prose} tfoot`, {
 
 globalStyle(`${prose} tfoot td`, {
   verticalAlign: 'top'
+})
+
+/*
+ * Sizes & Spacing
+ */
+globalStyle(prose, {
+  fontSize: rem(16),
+  lineHeight: round(28 / 16),
+  '@media': {
+    [media.lg]: {
+      fontSize: rem(18),
+      lineHeight: round(32 / 18)
+    }
+  }
+})
+
+globalStyle(`${prose} p`, {
+  marginTop: em(20, 16),
+  marginBottom: em(20, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(24, 18),
+      marginBottom: em(24, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} [class~="lead"]`, {
+  fontSize: em(20, 16),
+  lineHeight: round(32 / 20),
+  marginTop: em(24, 20),
+  marginBottom: em(24, 20),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(22, 18),
+      lineHeight: round(32 / 22),
+      marginTop: em(24, 22),
+      marginBottom: em(24, 22)
+    }
+  }
+})
+
+globalStyle(`${prose} blockquote`, {
+  marginTop: em(32, 20),
+  marginBottom: em(32, 20),
+  paddingLeft: em(20, 20),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(40, 24),
+      marginBottom: em(40, 24),
+      paddingLeft: em(24, 24)
+    }
+  }
+})
+
+globalStyle(`${prose} h1`, {
+  fontSize: em(36, 16),
+  marginTop: 0,
+  marginBottom: em(32, 36),
+  lineHeight: round(40 / 36),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(48, 18),
+      marginTop: 0,
+      marginBottom: em(40, 48),
+      lineHeight: round(48 / 48)
+    }
+  }
+})
+
+globalStyle(`${prose} h2`, {
+  fontSize: em(24, 16),
+  marginTop: em(48, 24),
+  marginBottom: em(24, 24),
+  lineHeight: round(32 / 24),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(30, 18),
+      marginTop: em(56, 30),
+      marginBottom: em(32, 30),
+      lineHeight: round(40 / 30)
+    }
+  }
+})
+
+globalStyle(`${prose} h3`, {
+  fontSize: em(20, 16),
+  marginTop: em(32, 20),
+  marginBottom: em(12, 20),
+  lineHeight: round(32 / 20),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(24, 18),
+      marginTop: em(40, 24),
+      marginBottom: em(16, 24),
+      lineHeight: round(36 / 24)
+    }
+  }
+})
+
+globalStyle(`${prose} h4`, {
+  marginTop: em(24, 16),
+  marginBottom: em(8, 16),
+  lineHeight: round(24 / 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(32, 18),
+      marginBottom: em(8, 18),
+      lineHeight: round(28 / 18)
+    }
+  }
+})
+
+globalStyle(`${prose} img`, {
+  marginTop: em(32, 16),
+  marginBottom: em(32, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(32, 18),
+      marginBottom: em(32, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} video`, {
+  marginTop: em(32, 16),
+  marginBottom: em(32, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(32, 18),
+      marginBottom: em(32, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} figure`, {
+  marginTop: em(32, 16),
+  marginBottom: em(32, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(32, 18),
+      marginBottom: em(32, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} figure > *`, {
+  marginTop: 0,
+  marginBottom: 0
+})
+
+globalStyle(`${prose} figcaption`, {
+  fontSize: em(14, 16),
+  lineHeight: round(20 / 14),
+  marginTop: em(12, 14),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(16, 18),
+      lineHeight: round(24 / 16),
+      marginTop: em(16, 16)
+    }
+  }
+})
+
+globalStyle(`${prose} code`, {
+  fontSize: em(14, 16),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(16, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} h2 code`, {
+  fontSize: em(21, 24),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(26, 30)
+    }
+  }
+})
+
+globalStyle(`${prose} h3 code`, {
+  fontSize: em(18, 20),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(21, 24)
+    }
+  }
+})
+
+globalStyle(`${prose} pre`, {
+  fontSize: em(14, 16),
+  lineHeight: round(24 / 14),
+  marginTop: em(24, 14),
+  marginBottom: em(24, 14),
+  borderRadius: rem(6),
+  paddingTop: em(12, 14),
+  paddingRight: em(16, 14),
+  paddingBottom: em(12, 14),
+  paddingLeft: em(16, 14),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(16, 18),
+      lineHeight: round(28 / 16),
+      marginTop: em(32, 16),
+      marginBottom: em(32, 16),
+      borderRadius: rem(6),
+      paddingTop: em(16, 16),
+      paddingRight: em(24, 16),
+      paddingBottom: em(16, 16),
+      paddingLeft: em(24, 16)
+    }
+  }
+})
+
+globalStyle(`${prose} ol`, {
+  marginTop: em(20, 16),
+  marginBottom: em(20, 16),
+  paddingLeft: em(26, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(24, 18),
+      marginBottom: em(24, 18),
+      paddingLeft: em(28, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} ul`, {
+  marginTop: em(20, 16),
+  marginBottom: em(20, 16),
+  paddingLeft: em(26, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(24, 18),
+      marginBottom: em(24, 18),
+      paddingLeft: em(28, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} li`, {
+  marginTop: em(8, 16),
+  marginBottom: em(8, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(12, 18),
+      marginBottom: em(12, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} ol > li`, {
+  paddingLeft: em(6, 16),
+  '@media': {
+    [media.lg]: {
+      paddingLeft: em(8, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} ul > li`, {
+  paddingLeft: em(6, 16),
+  '@media': {
+    [media.lg]: {
+      paddingLeft: em(8, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} > ul > li p`, {
+  marginTop: em(12, 16),
+  marginBottom: em(12, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(16, 18),
+      marginBottom: em(16, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} > ul > li > *:first-child`, {
+  marginTop: em(20, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(24, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} > ul > li > *:last-child`, {
+  marginBottom: em(20, 16),
+  '@media': {
+    [media.lg]: {
+      marginBottom: em(24, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} > ol > li > *:first-child`, {
+  marginTop: em(20, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(24, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} > ol > li > *:last-child`, {
+  marginBottom: em(20, 16),
+  '@media': {
+    [media.lg]: {
+      marginBottom: em(24, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} ul ul, ul ol, ol ul, ol ol`, {
+  marginTop: em(12, 16),
+  marginBottom: em(12, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(16, 18),
+      marginBottom: em(16, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} hr`, {
+  marginTop: em(48, 16),
+  marginBottom: em(48, 16),
+  '@media': {
+    [media.lg]: {
+      marginTop: em(56, 18),
+      marginBottom: em(56, 18)
+    }
+  }
+})
+
+globalStyle(`${prose} hr + *`, {
+  marginTop: 0
+})
+
+globalStyle(`${prose} h2 + *`, {
+  marginTop: 0
+})
+
+globalStyle(`${prose} h3 + *`, {
+  marginTop: 0
+})
+
+globalStyle(`${prose} h4 + *`, {
+  marginTop: 0
+})
+
+globalStyle(`${prose} table`, {
+  fontSize: em(14, 16),
+  lineHeight: round(24 / 14),
+  '@media': {
+    [media.lg]: {
+      fontSize: em(16, 18),
+      lineHeight: round(24 / 16)
+    }
+  }
+})
+
+globalStyle(`${prose} thead th`, {
+  paddingRight: em(8, 14),
+  paddingBottom: em(8, 14),
+  paddingLeft: em(8, 14),
+  '@media': {
+    [media.lg]: {
+      paddingRight: em(12, 16),
+      paddingBottom: em(12, 16),
+      paddingLeft: em(12, 16)
+    }
+  }
+})
+
+globalStyle(`${prose} thead th:first-child`, {
+  paddingLeft: 0
+})
+
+globalStyle(`${prose} thead th:last-child`, {
+  paddingRight: 0
+})
+
+globalStyle(`${prose} tbody td, tfoot td`, {
+  paddingTop: em(8, 14),
+  paddingRight: em(8, 14),
+  paddingBottom: em(8, 14),
+  paddingLeft: em(8, 14),
+  '@media': {
+    [media.lg]: {
+      paddingTop: em(12, 16),
+      paddingRight: em(12, 16),
+      paddingBottom: em(12, 16),
+      paddingLeft: em(12, 16)
+    }
+  }
+})
+
+globalStyle(`${prose} tbody td:first-child, tfoot td:first-child`, {
+  paddingLeft: 0
+})
+
+globalStyle(`${prose} tbody td:last-child, tfoot td:last-child`, {
+  paddingRight: 0
+})
+
+globalStyle(`${prose} > :first-child`, {
+  marginTop: 0
+})
+
+globalStyle(`${prose} > :last-child`, {
+  marginBottom: 0
 })
