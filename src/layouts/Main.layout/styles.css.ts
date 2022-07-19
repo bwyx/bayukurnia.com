09ba63c3d049d4f766d3d14acb8604a32a552b69
Bayu Kurnia <hi@bayukurnia.com>
2022-07-19 19:04:03 +0700

    ve: MainLayout

---
+++ src/layouts/Main.layout/styles.css.ts
@@ -0,0 +1,12 @@
+import { box } from '~/styles/sprinkles'
+
+const styles = {
+  main: box({
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    overflow: 'hidden'
+  })
+}
+
+export default styles
