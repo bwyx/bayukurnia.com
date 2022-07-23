7375531082750383a32895fff1e5003e7caacd32
Bayu Kurnia <hi@bayukurnia.com>
2022-07-23 23:23:00 +0700

    remove stitches code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { getCssText } from '~/styles'
 
 export default class Document extends NextDocument {
   render() {
@@ -14,7 +13,6 @@ export default class Document extends NextDocument {
             type="font/woff2"
             crossOrigin="true"
           />
-          <style dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
         <body>
           <script
