406ba8484a8f47117c1a4a52dbff38d0cb264ec9
Bayu Kurnia <bayu@qiscus.org>
2023-02-05 21:21:59 +0700

    added mdx support for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     plugins: [vanillaExtractPlugin()]
   }
