4b841bf04eb3658c28bcc21f0790e5b687bbc151
Bayu Kurnia <bayu@qiscus.org>
2023-02-05 12:34:09 +0700

    astro add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [vanillaExtractPlugin()]
   }
