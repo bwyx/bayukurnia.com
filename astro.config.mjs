faeeb36c9f8d3ebcfadfba0455748fc69093480a
Bayu Kurnia <bayu@qiscus.org>
2023-02-07 07:15:06 +0700

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
+import vercel from '@astrojs/vercel/serverless'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [react(), mdx()],
   vite: {
     plugins: [vanillaExtractPlugin()]
