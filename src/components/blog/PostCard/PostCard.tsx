b2db630b13cfc88dcbcdba6f1ba1e1c273ce847c
Bayu Kurnia <bayu@qiscus.org>
2023-02-07 07:15:38 +0700

    hotfix old next import dependencies

--- src/components/blog/PostCard/PostCard.tsx
+++ src/components/blog/PostCard/PostCard.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
 
 import type { PostProperties } from '~/types/blog.type'
@@ -13,15 +13,13 @@ const PostCard = ({
   slug
 }: PostProperties) => {
   return (
-    <Link href={`/${path}/${slug}`}>
-      <a className={styles.outer}>
-        <time className={styles.publishedAt} dateTime={publishedAt}>
-          {format(parseISO(publishedAt), 'LLLL d, yyyy')}
-        </time>
-        <h2 className={styles.title}>{title}</h2>
-        <p className={styles.summary}>{summary}</p>
-      </a>
-    </Link>
+    <a href={`/${path}/${slug}`} className={styles.outer}>
+      <time className={styles.publishedAt} dateTime={publishedAt}>
+        {format(parseISO(publishedAt), 'LLLL d, yyyy')}
+      </time>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.summary}>{summary}</p>
+    </a>
   )
 }
 
