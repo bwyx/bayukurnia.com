05b808daa365f4994870826034bd1bef1a12c1fa
Bayu Kurnia <hi@bayukurnia.com>
2023-02-07 07:30:05 +0700

    hotfix old next import dependencies

--- src/components/mdx/blog/vanilla-extract-css-di-typescript/index.ts
+++ src/components/mdx/blog/vanilla-extract-css-di-typescript/index.ts
@@ -1,10 +1,10 @@
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
 const components = {
-  ZeroRuntimeButton: dynamic(() => import('./ZeroRuntimeButton')),
-  SprinklesButton: dynamic(() => import('./SprinklesButton')),
-  VariantsButton: dynamic(() => import('./VariantsButton/VariantsButton')),
-  Buttons: dynamic(() => import('./VariantsButton'))
+  // ZeroRuntimeButton: dynamic(() => import('./ZeroRuntimeButton')),
+  // SprinklesButton: dynamic(() => import('./SprinklesButton')),
+  // VariantsButton: dynamic(() => import('./VariantsButton/VariantsButton')),
+  // Buttons: dynamic(() => import('./VariantsButton'))
 }
 
 export default components
