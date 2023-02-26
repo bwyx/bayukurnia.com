8510fb12624c260a06e6765ae2e0fe2f39f25471
Bayu Kurnia <hi@bayukurnia.com>
2023-02-26 19:48:16 +0700

    cf ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare({ mode: 'advanced' }),
   integrations: [react(), mdx()],
   vite: {
     plugins: [vanillaExtractPlugin()]
