8efd49a6abe9d63c8fdf7b3ff2ad6b0b71263dc3
Bayu Kurnia <hi@bayukurnia.com>
2023-03-02 07:38:51 +0700

    use gsap on logo animation

--- src/components/vectors/Logo/Logo.css.ts
+++ src/components/vectors/Logo/Logo.css.ts
@@ -1,60 +1,23 @@
-import { globalStyle, keyframes, style } from '@vanilla-extract/css'
-import { media } from '~/styles/variables.css'
+import { style } from '@vanilla-extract/css'
+import { rgb } from '~/styles/variables.css'
 
-const drawPath = keyframes({
-  '0%': { strokeDashoffset: 300 },
-  '100%': { strokeDashoffset: 0 }
+const bayuKurnia = style({ position: 'relative' })
+
+const logo = style({
+  width: 'auto',
+  height: 40,
+  color: `rgb(${rgb.brand})`
 })
 
-const animatedLogo = style({
-  '@media': {
-    [media.motionSafe]: {
-      strokeDasharray: 300,
-      strokeDashoffset: 300,
-      willChange: 'stroke-dashoffset'
-    }
-  }
+const strokeBg = style({
+  width: 'auto',
+  height: 40,
+  inset: 0,
+  opacity: 0.25,
+  position: 'absolute',
+  pointerEvents: 'none',
+  color: `rgb(${rgb.fg3})`
 })
 
-globalStyle(`${animatedLogo} [name=b]`, {
-  animation: `${drawPath} 0.8s linear forwards`,
-  animationDelay: '0.5s'
-})
-
-globalStyle(`${animatedLogo} [name=ayu]`, {
-  animation: `${drawPath} 1s ease-in-out forwards`,
-  animationDelay: '0.8s'
-})
-
-globalStyle(`${animatedLogo} [name=k1]`, {
-  animation: `${drawPath} 0.4s ease-in-out forwards`,
-  animationDelay: '1.8s'
-})
-
-globalStyle(`${animatedLogo} [name=k2]`, {
-  animation: `${drawPath} 0.6s ease-in-out forwards`,
-  animationDelay: '2s'
-})
-
-globalStyle(`${animatedLogo} [name=ur]`, {
-  animation: `${drawPath} 0.8s linear forwards`,
-  animationDelay: '2.2s'
-})
-
-globalStyle(`${animatedLogo} [name=ni]`, {
-  animation: `${drawPath} 0.7s linear forwards`,
-  animationDelay: '2.6s'
-})
-
-globalStyle(`${animatedLogo} [name=dot]`, {
-  animation: `${drawPath} 0.3s linear forwards`,
-  animationDelay: '3.2s'
-})
-
-globalStyle(`${animatedLogo} [name=a]`, {
-  animation: `${drawPath} 1s linear forwards`,
-  animationDelay: '2.8s'
-})
-
-const styles = { animatedLogo }
+const styles = { logo, strokeBg, bayuKurnia }
 export default styles
