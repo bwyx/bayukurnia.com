56e2080de2329fdacdd2992b1ac71509f0e3f31b
Bayu Kurnia <hi@bayukurnia.com>
2023-03-11 21:39:40 +0700

    remove unused code

--- src/components/icons/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SendIcon } from './Send.icon'
-export { default as MessageIcon } from './Message.icon'
--- src/components/mdx/Pre/Pre.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Pre.css'
-
-const Pre = ({ children }: { children: React.ReactNode }) => {
-  return <pre className={styles.prism}>{children}</pre>
-}
-
-export default Pre
