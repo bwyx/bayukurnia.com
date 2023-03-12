dec65c15d4f76043179af0a3fc3a11e3647f5f91
Bayu Kurnia <hi@bayukurnia.com>
2023-03-12 19:25:00 +0700

    solid: NowPlaying

--- src/components/NowPlaying/NowPlaying.tsx
+++ src/components/NowPlaying/NowPlaying.tsx
@@ -1,72 +1,105 @@
-import useSWR from 'swr'
+import { createResource, onMount, onCleanup, Show } from 'solid-js'
+import { isServer } from 'solid-js/web'
 
 import SpotifyLogo from '~/components/vectors/SpotifyLogo'
 
 import styles from './NowPlaying.css'
 
-import type { NowPlayingResponse } from '~/types/spotify.type'
+import type { NowPlayingResponse, MusicPlaying } from '~/types/spotify.type'
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+const getCurrentlyPlaying = () =>
+  fetch('/api/now-playing').then<NowPlayingResponse>((res) => res.json())
 
 const Bars = () => (
-  <div className={styles.bars}>
-    <span className={styles.barItem} />
-    <span className={styles.barItem} />
-    <span className={styles.barItem} />
-    <span className={styles.barItem} />
+  <div class={styles.bars}>
+    <span class={styles.barItem} />
+    <span class={styles.barItem} />
+    <span class={styles.barItem} />
+    <span class={styles.barItem} />
   </div>
 )
 
-const NowPlaying = () => {
-  const { data: music } = useSWR<NowPlayingResponse>(
-    '/api/now-playing',
-    fetcher
-  )
+const Placeholder = (props: { loading: boolean }) => (
+  <div class={styles.inner}>
+    <div class={styles.player}>
+      <div class={styles.cover}>
+        <SpotifyLogo />
+      </div>
+      <div class={styles.details}>
+        <p class={styles.title}>Spotify</p>
+        <span class={styles.artists}>
+          {props.loading ? 'Loading...' : 'Not Playing'}
+        </span>
+      </div>
+    </div>
+  </div>
+)
 
-  const Component = music?.isPlaying ? 'a' : 'div'
+export default function () {
+  const [music, { refetch }] = createResource(getCurrentlyPlaying)
+  const musicPlaying = music as () => MusicPlaying
+
+  if (!isServer) {
+    const focusThrottleInterval = 5_000
+    const refreshInterval = 30_000
+
+    let polling: ReturnType<typeof setTimeout>
+    let lastFetchedOn = Date.now()
+
+    const checkShouldRefetch = (delta: number) => {
+      const now = Date.now()
+
+      if (now >= lastFetchedOn + delta) {
+        lastFetchedOn = now
+        refetch()
+      }
+    }
+
+    const handleIntervalFetch = () => checkShouldRefetch(refreshInterval)
+    const handleFocusFetch = () => checkShouldRefetch(focusThrottleInterval)
+
+    onMount(() => {
+      polling = setInterval(handleIntervalFetch, refreshInterval)
+      window.addEventListener('focus', handleFocusFetch)
+    })
+
+    onCleanup(() => {
+      clearInterval(polling)
+      window.removeEventListener('focus', handleFocusFetch)
+    })
+  }
 
   return (
-    <Component
-      href={music?.isPlaying ? music.spotifyUrl : undefined}
-      className={styles.inner}
-      target="_blank"
-      rel="noreferrer"
+    <Show
+      when={music()?.isPlaying}
+      fallback={<Placeholder loading={music.loading} />}
     >
-      <div className={styles.player}>
-        <div className={styles.cover}>
-          {music?.isPlaying ? (
+      <a
+        href={musicPlaying().spotifyUrl}
+        class={styles.inner}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div class={styles.player}>
+          <div class={styles.cover}>
             <img
-              src={music.cover}
-              srcSet={music.srcset}
-              alt={`[${music.title}] Cover Album`}
+              src={musicPlaying().cover}
+              srcSet={musicPlaying().srcset}
+              alt={`[${musicPlaying().title}] Cover Album`}
               height={42}
               width={42}
             />
-          ) : (
-            <SpotifyLogo />
-          )}
-        </div>
-        <div className={styles.details}>
-          <p className={styles.title}>
-            {music?.isPlaying ? music.title : 'Spotify'}
-          </p>
-          {music?.isPlaying ? (
-            <>
-              <span className={`${styles.artists} artists`}>
-                {music.artists}
-              </span>
-              <span className={`${styles.artists} play`}>Play on Spotify</span>
-            </>
-          ) : (
-            <span className={styles.artists}>
-              {music ? 'Not Playing' : 'Loading...'}
+          </div>
+          <div class={styles.details}>
+            <p class={styles.title}>{musicPlaying().title}</p>
+            <span class={`${styles.artists} artists`}>
+              {musicPlaying().artists}
             </span>
-          )}
+            <span class={`${styles.artists} play`}>Play on Spotify</span>
+          </div>
         </div>
-      </div>
-      {music?.isPlaying ? <Bars /> : null}
-    </Component>
+        <Bars />
+      </a>
+    </Show>
   )
 }
-
-export default NowPlaying
