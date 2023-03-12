3288f9509a7ee778e8c56cbe1190ac52ef875380
Bayu Kurnia <hi@bayukurnia.com>
2023-03-12 23:22:58 +0700

    env typings

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,17 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ENV {
+  readonly SITE_URL: string
+
+  readonly CLOUDINARY_CLOUD_NAME: string
+  readonly CLOUDINARY_FOLDER: string
+
+  readonly SPOTIFY_CLIENT_ID: string
+  readonly SPOTIFY_CLIENT_SECRET: string
+  readonly SPOTIFY_REFRESH_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ENV
+}
