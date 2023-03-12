a3950cf5df0fc52ebc6ba49dac892f6e17358871
Bayu Kurnia <hi@bayukurnia.com>
2023-03-12 19:52:47 +0700

    re-add react, for content components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
 import solid from '@astrojs/solid-js'
+import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 
 import gruvbox from './gruvbox-material-dark.json'
@@ -12,7 +13,7 @@ export default defineConfig({
   site: 'https://bayukurnia.com',
   output: 'server',
   adapter: cloudflare({ mode: 'advanced' }),
-  integrations: [mdx(), sitemap(), solid()],
+  integrations: [mdx(), sitemap(), solid(), react()],
   build: { assets: '_' },
   markdown: {
     shikiConfig: {
