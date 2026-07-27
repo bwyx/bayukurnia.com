5ca85bc4827c9bc79bec149072a972bed20b34e7
Bayu Kurnia <hi@bayukurnia.com>
2026-07-27 21:42:26 +0700

    fix: now playing env

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -1,5 +1,4 @@
 import { getAccessToken, getCurrentlyPlaying, Auth } from '~/lib/spotify'
-import { getRuntime } from '@astrojs/cloudflare/runtime'
 
 import { CloudinaryImage } from '@cloudinary/url-gen'
 import { fill } from '@cloudinary/url-gen/actions/resize'
@@ -7,29 +6,15 @@ import { fill } from '@cloudinary/url-gen/actions/resize'
 import type { APIRoute } from 'astro'
 import type { NowPlayingResponse } from '~/types/spotify.type'
 
-export const get: APIRoute = async ({ request }) => {
-  const cloudflareRuntime = getRuntime<ImportMetaEnv>(request)
-
-  let cloudName: string
-  let auth: Auth
-
-  if (cloudflareRuntime) {
-    cloudName = cloudflareRuntime.env.CLOUDINARY_CLOUD_NAME
-    auth = {
-      clientId: cloudflareRuntime.env.SPOTIFY_CLIENT_ID,
-      clientSecret: cloudflareRuntime.env.SPOTIFY_CLIENT_SECRET,
-      refreshToken: cloudflareRuntime.env.SPOTIFY_REFRESH_TOKEN
-    }
-  } else {
-    cloudName = import.meta.env.CLOUDINARY_CLOUD_NAME
-    auth = {
-      clientId: import.meta.env.SPOTIFY_CLIENT_ID,
-      clientSecret: import.meta.env.SPOTIFY_CLIENT_SECRET,
-      refreshToken: import.meta.env.SPOTIFY_REFRESH_TOKEN
-    }
+export const get: APIRoute = async () => {
+  const cloudName = import.meta.env.CLOUDINARY_CLOUD_NAME
+  const auth: Auth = {
+    clientId: import.meta.env.SPOTIFY_CLIENT_ID,
+    clientSecret: import.meta.env.SPOTIFY_CLIENT_SECRET,
+    refreshToken: import.meta.env.SPOTIFY_REFRESH_TOKEN
   }
 
-  const accessToken = await getAccessToken(auth, cloudflareRuntime?.env.CACHE)
+  const accessToken = await getAccessToken(auth)
   const music = await getCurrentlyPlaying(accessToken)
 
   let data: NowPlayingResponse
